6ef7edfa5aece56464428ab369bb2e9682578d2e
Anup Gupta <anuup22.cs@gmail.com>
2024-12-08 00:51:12 +0530

    feat: update title to use signal for reactive state management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   imports: [HeaderComponent],
   template: `
-    <h1>Welcome to {{ title }}!</h1>
+    <h1>Welcome to {{ title() }}!</h1>
 
     <app-header />
   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'angular-ecommerce';
+  title = signal('Angular Ecommerce');
 }
