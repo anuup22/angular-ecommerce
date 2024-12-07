8cabf40b71f7824f4d457661a6377e73c0c9bb05
Anup Gupta <anuup22.cs@gmail.com>
2024-12-08 00:57:28 +0530

    feat: enhance header component with styling and structure

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,8 +3,14 @@ import { Component, signal } from '@angular/core';
 @Component({
   selector: 'app-header',
   imports: [],
-  template: ` <h1>Welcome to {{ title() }}!</h1> `,
-  styles: ``,
+  template: ` <div class="header">Welcome to {{ title() }}!</div> `,
+  styles: `
+  .header {
+    background-color: #333;
+    color: #fff;
+    padding: 10px;
+    text-align: center;
+  }`,
 })
 export class HeaderComponent {
   title = signal('Angular Ecommerce!');
